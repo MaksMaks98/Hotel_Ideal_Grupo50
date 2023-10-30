61638967dd6566d3ad2e13f8d95105a5fc7a30d6
MaksMaks98 <141648316+MaksMaks98@users.noreply.github.com>
2023-10-29 22:06:18 -0300

    Base de datos ACTUALIZADA

--- hotel_ideal.sql
+++ hotel_ideal.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 06-10-2023 a las 23:44:29
+-- Tiempo de generación: 30-10-2023 a las 02:05:01
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -43,7 +43,12 @@ CREATE TABLE `categoria` (
 --
 
 INSERT INTO `categoria` (`idCategoria`, `nomCategoria`, `cantidadCamas`, `cantPersonas`, `tipoCama`, `precio`) VALUES
-(9, 'Estándar', 3, 3, '1 cama matrimonial y 1 cama individual', 10000);
+(9, 'Estándar', 1, 2, '1 cama matrimonial ', 10000),
+(10, 'Simple', 1, 1, '1 cama individual', 6000),
+(11, 'Doble', 2, 2, '2 individuales', 6000),
+(12, 'Triple', 2, 3, '1 cama matrimonial y 1 individual', 8000),
+(13, 'Cuádruple', 3, 4, '1 cama matrimonial y 2 individuales', 14000),
+(14, 'Suite Lujo', 1, 2, '1 cama king-size', 14000);
 
 -- --------------------------------------------------------
 
@@ -64,8 +69,15 @@ CREATE TABLE `habitacion` (
 --
 
 INSERT INTO `habitacion` (`idHabitacion`, `numHabitacion`, `idCategoria`, `piso`, `estado`) VALUES
-(1, 13, 9, 1, 1),
-(2, 13, 9, 1, 1);
+(1, 213, 10, 3, 0),
+(2, 513, 11, 5, 1),
+(3, 413, 9, 4, 0),
+(4, 10, 9, 3, 1),
+(6, 9, 13, 0, 0),
+(7, 1, 9, 0, 0),
+(10, 4, 13, 0, 0),
+(11, 7, 13, 0, 0),
+(12, 120, 12, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -90,8 +102,14 @@ CREATE TABLE `huesped` (
 
 INSERT INTO `huesped` (`idHuesped`, `nombre`, `apellido`, `dni`, `domicilio`, `correo`, `celular`, `estado`) VALUES
 (2, 'Pablo', 'Estevanez', '23345567', 'La Plata', 'Pablo_example@gmail.com', '456789087', 0),
-(3, 'Ciro', 'Del Barco', '234666786', 'Córdoba', 'Ciro_example@gmail.com', '455589087', 1),
-(4, 'María', 'Luisa', '345666789', 'Jujuy', 'MariaL_example@gmail.com', '234555677', 1);
+(3, 'Ciro', 'Del Barco', '234666786', 'Córdoba', 'Ciro_example@gmail.com', '234555677', 1),
+(4, 'María', 'Luisa', '345666789', 'La Pampa', 'MariaL_example@gmail.com', '234555677', 0),
+(5, 'Teresa', 'Barrionuevo', '4446786', 'Tucuman', 'Teresa_example@gmail.com', '555577', 1),
+(7, 'Elsa', 'Rivero', '678886', 'San Luis', 'Elsa_example@gmail.com', '6666577', 0),
+(11, 'Rosa', 'Del Valle', '34555234', 'Catamarca', 'Rosa_example@gmail.com', '234233334', 0),
+(16, '', '', '23345568', 'La Plata', 'Pablo_example@gmail.com', '456789087', 0),
+(18, '', '', '23345564', 'La Plata', 'Pablo_example@gmail.com', '456789087', 0),
+(19, '', '', '23345561', 'La Plata', 'Pablo_example@gmail.com', '456789087', 0);
 
 -- --------------------------------------------------------
 
@@ -111,6 +129,18 @@ CREATE TABLE `reserva` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Volcado de datos para la tabla `reserva`
+--
+
+INSERT INTO `reserva` (`idReserva`, `idHuesped`, `idHabitacion`, `cantidadPersona`, `fechaIngreso`, `fechaSalida`, `monto`, `estado`) VALUES
+(1, 5, 3, 3, '2023-04-21', '2023-04-25', 10000, 0),
+(2, 5, 2, 3, '2023-04-13', '2023-04-30', 102000, 1),
+(3, 3, 4, 3, '2023-02-10', '2023-05-02', 40000, 1),
+(10, 5, 4, 3, '2023-11-30', '2023-12-25', 250000, 0),
+(11, 4, 1, 1, '2023-10-27', '2023-10-29', 12000, 1),
+(12, 4, 3, 2, '2023-10-27', '2023-11-08', 120000, 1);
+
+--
 -- Índices para tablas volcadas
 --
 
@@ -126,6 +156,7 @@ ALTER TABLE `categoria`
 --
 ALTER TABLE `habitacion`
   ADD PRIMARY KEY (`idHabitacion`),
+  ADD UNIQUE KEY `numHabitacion` (`numHabitacion`),
   ADD KEY `idCategoria` (`idCategoria`);
 
 --
@@ -151,25 +182,25 @@ ALTER TABLE `reserva`
 -- AUTO_INCREMENT de la tabla `categoria`
 --
 ALTER TABLE `categoria`
-  MODIFY `idCategoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `idCategoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT de la tabla `habitacion`
 --
 ALTER TABLE `habitacion`
-  MODIFY `idHabitacion` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `idHabitacion` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT de la tabla `huesped`
 --
 ALTER TABLE `huesped`
-  MODIFY `idHuesped` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `idHuesped` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT de la tabla `reserva`
 --
 ALTER TABLE `reserva`
-  MODIFY `idReserva` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idReserva` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- Restricciones para tablas volcadas
